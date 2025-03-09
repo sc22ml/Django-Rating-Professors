9fbafe152e8e8f9972627486e3ffda5c960e160f
sc22yfml <sc22yfml@leeds.ac.uk>
2025-03-09 20:08:19 +0000

    static

--- professor_rating/settings.py
+++ professor_rating/settings.py
@@ -85,6 +85,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
